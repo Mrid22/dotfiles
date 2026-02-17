b0531c5a1cd007e7f1b43a39aee9bdec831fd088
Mrid22 <mridulaga@outlook.com>
2026-02-17 11:56:54 +0200

    nvim(feat): added keybind for live grep

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,6 +10,7 @@ vim.g.mapleader = " "
 -- Shortcuts
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>")
+vim.keymap.set("n", "<leader>fg", ":FzfLua grep_visual<CR>")
 vim.keymap.set("n", "<leader>e",  ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>ca",  ":Lspsaga code_action<CR>")
 
