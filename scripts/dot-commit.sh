6adeebc0995da564943444fe87fd6c71ef650176
Mrid22 <mridulaga@outlook.com>
2026-02-17 18:22:29 +0200

    added gh config

--- scripts/dot-commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
-SCOPE=$(/usr/bin/ls | gum choose)
-DESCRIPTION=$(gum input)
-
-git add .
-git commit -a -m "$TYPE($SCOPE): $DESCRIPTION"
