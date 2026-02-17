d14962db9b0a3341a54084be595f470594aa0ec7
Mrid22 <mridulaga@outlook.com>
2026-02-17 17:50:19 +0200

    nvim: more lspsaga features

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,6 +15,8 @@ vim.keymap.set("n", "<leader>fb", ":FzfLua buffers<CR>")
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>")
 vim.keymap.set("n", "<leader>ca", ":FzfLua lsp_code_actions<CR>")
+vim.keymap.set("n", "<leader>rn", ":Lspsaga rename<CR>")
+vim.keymap.set("n", "<leader>K", ":Lspsaga hover_doc<CR>")
 
 -- Plugins
 local gh = function(x) return "https://github.com/" .. x end
@@ -59,6 +61,9 @@ vim.pack.add({
 	},
 	{
 		src = gh("MysticalDevil/inlay-hints.nvim")
+	},
+	{
+		src = gh("onsails/lspkind.nvim")
 	},
 	-- Fzf
 	{
@@ -171,6 +176,7 @@ null_ls.setup({
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp') -- autopairs with completion for functions and such
 local cmp = require("cmp")
+local lspkind = require('lspkind')
 cmp.event:on(
 	'confirm_done',
 	cmp_autopairs.on_confirm_done()
@@ -181,6 +187,27 @@ cmp.setup({
 			vim.snippet.expand(args.body)
 		end,
 	},
+	  formatting = {
+    fields = { 'abbr', 'icon', 'kind', 'menu' },
+    format = lspkind.cmp_format({
+      maxwidth = {
+        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        -- can also be a function to dynamically calculate max width such as
+        -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+        menu = 50, -- leading text (labelDetails)
+        abbr = 50, -- actual suggestion item
+      },
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        -- ...
+        return vim_item
+      end
+    })
+  },
 	window = {
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
